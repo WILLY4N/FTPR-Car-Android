b2de747a75362617ea654a967c16b2437fe1dd3e
Willyan <willyan2006@gmail.com>
2026-03-20 18:15:49 -0300

    Adicionando localização no addItem

--- app/src/main/java/com/example/myapitest/NewItemActivity.kt
+++ app/src/main/java/com/example/myapitest/NewItemActivity.kt
@@ -1,30 +1,87 @@
 package com.example.myapitest
 
+import android.Manifest
+import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.example.myapitest.databinding.ActivityNewItemBinding
 import com.example.myapitest.model.ItemCar
+import com.example.myapitest.model.ItemPlace
 import com.example.myapitest.service.Result
 import com.example.myapitest.service.RetrofitClient
 import com.example.myapitest.service.safeApiCall
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
+import com.google.android.gms.maps.model.MarkerOptions
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import java.security.SecureRandom
 
-class NewItemActivity : AppCompatActivity() {
+class NewItemActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var binding: ActivityNewItemBinding
+
+    private lateinit var mMap: GoogleMap
+    private var selectedMarker: Marker? = null
+
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityNewItemBinding.inflate(layoutInflater)
         setContentView(binding.root)
         setupView()
+        requestLocationPermission()
+        setupGoogleMap()
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+        mMap.uiSettings.isZoomControlsEnabled = true
+        binding.mapContent.visibility = View.VISIBLE
+        getDeviceLocation()
+        mMap.setOnMapClickListener { latLng ->
+            selectedMarker?.remove()
+            selectedMarker = mMap.addMarker(
+                MarkerOptions()
+                    .position(latLng)
+                    .draggable(true)
+                    .title("Lat: ${latLng.latitude}, Lng: ${latLng.longitude}")
+            )
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        when(requestCode){
+            LOCATION_PERMISSION_REQUEST_CODE -> {
+                if(grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    loadCurrentLocation()
+                } else {
+                    Toast.makeText(this, R.string.location_permission_denied, Toast.LENGTH_LONG).show()
+                }
+            }
+        }
     }
 
     private fun setupView() {
@@ -37,8 +94,45 @@ class NewItemActivity : AppCompatActivity() {
         binding.saveCta.setOnClickListener { saveCar() }
     }
 
+    @SuppressLint("MissingPermission")
+    private fun requestLocationPermission() {
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        fusedLocationClient.lastLocation.addOnSuccessListener { location ->
+            location?.let {
+                val latLong = LatLng(it.latitude, it.longitude)
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLong, 16f))
+            }
+        }
+    }
+
+    private fun setupGoogleMap() {
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    private fun getDeviceLocation(){
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+            loadCurrentLocation()
+        } else {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), LOCATION_PERMISSION_REQUEST_CODE
+            )
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun loadCurrentLocation(){
+        mMap.isMyLocationEnabled = true
+        mMap.uiSettings.isMyLocationButtonEnabled = true
+        mMap.uiSettings.isCompassEnabled = true
+        fusedLocationClient
+    }
+
     private fun saveCar() {
         if (!validateForm()) return
+
+        val itemPlace = selectedMarker?.position?.let {
+            ItemPlace(it.latitude, it.longitude)
+        }
 
         CoroutineScope(Dispatchers.IO).launch {
             val id = SecureRandom().nextInt().toString()
@@ -48,15 +142,14 @@ class NewItemActivity : AppCompatActivity() {
                 year = binding.year.text.toString(),
                 licence = binding.licence.text.toString(),
                 imageUrl = binding.imageUrl.text.toString(),
-                place = null
+                place = itemPlace
             )
 
             val result = safeApiCall { RetrofitClient.apiService.addItem(itemCar) }
             withContext(Dispatchers.Main) {
                 when (result) {
                     is Result.Success -> handleOnSuccess()
-
-                    is Result.Error -> handleOnError()
+                    is Result.Error  -> handleOnError()
                 }
             }
         }
@@ -102,6 +195,9 @@ class NewItemActivity : AppCompatActivity() {
     }
 
     companion object {
+
+        private const val LOCATION_PERMISSION_REQUEST_CODE = 1001
+
         fun newIntent(context: Context): Intent {
             return Intent(context, NewItemActivity::class.java)
         }
